8f559bb82f98e71be57dc6555ca6aa29d45989c5
Thiago Dantas <thiago.teixeira.dantas@gmail.com>
2013-09-04 23:12:48 +0100

    EM Signal Trap

--- lib/linkedcare/launcher.rb
+++ lib/linkedcare/launcher.rb
@@ -4,9 +4,14 @@ module Linkedcare
     def start
 
       EM.run do 
+        
+        Signal.trap("INT")  { puts "int"; EventMachine.stop }
+        Signal.trap("TERM") { puts "TERM"; EventMachine.stop }
+
         AMQP.connect(Linkedcare::Bus::Configurable.amqp_options) do |connection|
           Linkedcare::Bus::ConnectionHandler.new(connection).setup
         end
+        
       end
 
     end
