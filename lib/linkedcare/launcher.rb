46d29696a831ac85acb33e4c186ba3bf7a798e3c
Thiago Dantas <thiago.teixeira.dantas@gmail.com>
2013-09-04 21:37:41 +0100

    testing

--- lib/linkedcare/launcher.rb
+++ lib/linkedcare/launcher.rb
@@ -2,16 +2,18 @@ module Linkedcare
   module Launcher
     
     def start
+
       EM.run do 
         AMQP.connect(Linkedcare::Bus::Configurable.amqp_options) do |connection|
           Linkedcare::Bus::ConnectionHandler.new(connection).setup
         end
       end
+
     end
 
     def stop
       EventMachine.stop
     end
-    
+
   end
 end
